9ff439b29d2d1657c39102b405663aa5c5efdb8d
hgullapa <62145510+hgullapa@users.noreply.github.com>
2020-08-21 14:04:45 -0400

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,7 +1,8 @@
-import os
-def test_sample_true():
-    assert True
-    secret = os.getenv('super_secret')
-    print("secret")
-    print(secret)
-   
+num1 = 15
+num2 = 12
+  
+# Adding two nos 
+sum = num1 + num2 
+  
+# printing values 
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum)) 
