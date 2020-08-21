3d16478107b3bffde07742d8a2e78b2567970e5f
hgullapa <62145510+hgullapa@users.noreply.github.com>
2020-08-21 14:09:00 -0400

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,3 +1,5 @@
+import os
+
 num1 = 15
 num2 = 12
   
@@ -6,3 +8,6 @@ sum = num1 + num2
   
 # printing values 
 print("Sum of {0} and {1} is {2}" .format(num1, num2, sum)) 
+secret = os.getenv('super_secret') 
+print(secret)
+  
