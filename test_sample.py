3eefb6d57ccbdd2e36083c05d7d927e4acbc7469
hgullapa <62145510+hgullapa@users.noreply.github.com>
2020-08-28 13:07:23 -0400

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,13 +1,18 @@
 import os
+import sys
 
-num1 = 18
-num2 = 12
+def main(secret):
+
+    num1 = 18
+    num2 = 12
   
-# Adding two nos 
-sum = num1 + num2 
+    # Adding two nos 
+    sum = num1 + num2 
   
-# printing values 
-print("Sum of {0} and {1} is {2}" .format(num1, num2, sum)) 
-secret = os.getenv('super_secret') 
-print(secret)
+    # printing values 
+    print("Sum of {0} and {1} is {2}" .format(num1, num2, sum)) 
+    if secret==25:
+       print("I am 25")
+if __name__ == "__main__":
+    main(sys.argv[1])
   
